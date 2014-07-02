4561fff4501192cfebdafeea8cfae4eae07bc1d3
unknown <Anoop.PT@DMC1168L.emaratech.ae>
2014-07-02 14:19:12 +0400

    firstCommit

---
+++ exp1.hs
@@ -0,0 +1,12 @@
+doubleMe x = x + x
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]   
+
+length' xs = sum [1 | _ <- xs]  
+
+factorial :: Integer -> Integer  
+factorial n = product [1..n]  
+
+lucky :: (Integral a) => a -> String  
+lucky 7 = "LUCKY NUMBER SEVEN!"  
+lucky x = "Sorry, you're out of luck, pal!"   
